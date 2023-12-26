051661d81e5b3bb7794d13e0fac62625a004c780
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-12-26 12:53:56 -0300

    Classe de frame dinamica

--- Testes/testeTela/janela.py
+++ Testes/testeTela/janela.py
@@ -19,24 +19,21 @@ import customtkinter
 # app.mainloop()
 
 class MyCheckboxFrame(customtkinter.CTkFrame):
-    def __init__(self, master):
+    def __init__(self, master, values):
         super().__init__(master)
-
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
-        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
-        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.checkbox_3 = customtkinter.CTkCheckBox(self, text="checkbox 3")
-        self.checkbox_3.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.values = values
+        self.checkboxes = []
+        
+        for i, value in enumerate(self.values):
+            checkbox = customtkinter.CTkCheckBox(self, text=value)
+            checkbox.grid(row=i, column=0, padx=10, pady=(10, 0), sticky="w")
+            self.checkboxes.append(checkbox)
     
     def get(self):
         checked_checkboxes = []
-        if self.checkbox_1.get() == 1:
-            checked_checkboxes.append(self.checkbox_1.cget("text"))
-        if self.checkbox_2.get() == 1:
-            checked_checkboxes.append(self.checkbox_2.cget("text"))
-        if self.checkbox_3.get() == 1:
-            checked_checkboxes.append(self.checkbox_3.cget("text"))
+        for checkbox in self.checkboxes:
+            if checkbox.get() == 1:
+                checked_checkboxes.append(checkbox.cget("text"))
         return checked_checkboxes
 
 class App(customtkinter.CTk):
@@ -48,7 +45,7 @@ class App(customtkinter.CTk):
         self.grid_columnconfigure(0, weight=1)
         self.grid_rowconfigure(0, weight=1)
 
-        self.checkbox_frame = MyCheckboxFrame(self)
+        self.checkbox_frame = MyCheckboxFrame(self, values=["value 1", "value 2", "value 3"])
         self.checkbox_frame.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="nsw")
 
         self.button = customtkinter.CTkButton(self, text="my button", command=self.button_callback)
