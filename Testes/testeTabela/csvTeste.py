64f5479fcbb1e65a9bf7992578c035c14c8e22d6
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-12-26 17:24:52 -0300

    Entendendo sobre manipulação de CSV com pandas e csv

--- Testes/testeTabela/csvTeste.py
+++ Testes/testeTabela/csvTeste.py
@@ -1,11 +1,16 @@
 import csv
 import pandas as pd
 
-#with open("C:/Users/DESOUR10/Downloads/_Aviso de Férias - Mensalistas - Layout_Python (2).csv", "r") as arquivo:
- #   arquivo_csv = csv.reader(arquivo, delimiter=",")
-  #  for i, linha in enumerate(arquivo_csv):
-   #     print(linha)
-        
 tabela = pd.read_csv("C:/Users/DESOUR10/Downloads/_Aviso de Férias - Mensalistas - Layout_Python (2).csv")
 print(tabela)
-print(tabela["ID"].sum())
+
+with open('C:/Users/DESOUR10/Desktop/Desenvolvimento/Testes/testeTabela/aviso.csv') as csv_file:
+   csv_reader = csv.DictReader(csv_file)
+   
+   with open('new_aviso.csv', "w") as new_file:
+      fieldnames = ['RE Colaborador','ID','Nome Destinatario','Email Destinatario', 'Status']
+      csv_writer = csv.DictWriter(new_file, fieldnames=fieldnames, delimiter=',')
+      csv_writer.writeheader()
+
+      for line in csv_reader:
+         csv_writer.writerow(line)
