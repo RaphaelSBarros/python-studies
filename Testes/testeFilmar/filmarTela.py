3178eceb4af77d7982c0f17e59afad36adfe446c
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-12-27 09:07:03 -0300

    Entendendo como é feito a gravação de telas

---
+++ Testes/testeFilmar/filmarTela.py
@@ -0,0 +1,24 @@
+import cv2
+import keyboard
+import pyautogui
+import numpy as np
+
+fps = 30 #define quantos prints serão tirados por segundo da tela
+tamanho_tela = tuple(pyautogui.size()) #define a resolução do vídeo baseado na própria tela
+
+codec = cv2.VideoWriter_fourcc(*"XVID") #codificador do vídeo AVI
+video = cv2.VideoWriter("video.avi", codec, fps, tamanho_tela) #nome do vídeo, codificador, taxa de quadros e resolução
+
+while True:
+    frame = pyautogui.screenshot() #tira 30 prints por segundo da tela
+    frame = np.array(frame) #armazena os prints em um array
+    
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR) #Define as cores para o padrão do cv2
+    
+    video.write(frame) #junta as imagens em um vídeo
+    
+    if keyboard.is_pressed("esc"):
+        break
+
+video.release() #cria o vídeo
+cv2.destroyAllWindows()
