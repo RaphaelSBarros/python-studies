72b83c331634a8374eea47a71680cd670f955ca9
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-12-27 16:28:34 -0300

    editando um pouco o gráfico

--- Testes/testeGrafico/barra.py
+++ Testes/testeGrafico/barra.py
@@ -3,6 +3,11 @@ import plotly.express as px
 dados_x=['2018', '2019', '2020', '2021']
 dados_y=[10, 20, 5, 35]
 
-fig=px.bar(x=dados_x, y=dados_y, width=700, height=300)
+fig=px.bar(x=dados_x, y=dados_y, title='Teste Gráfico', width=800, height=500)
+
+fig.update_yaxes(title='vertical') #muda o título do eixo
+fig.update_xaxes(title='horizontal')
+
+fig.update_traces(text=dados_y) #insere os dados dentro do gráfico
 
 fig.show()
