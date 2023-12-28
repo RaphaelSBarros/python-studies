723b81fefec9ff3a7b733d9fbbbec785474c9aed
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2023-12-28 08:13:33 -0300

    classes

---
+++ Testes/testeClasse/classe.py
@@ -0,0 +1,13 @@
+class ControleRemoto: #criando classe / self. = this.
+    def __init__(self, cor, altura, profundidade, largura):
+        self.cor=cor
+        self.altura=altura
+        self.profundidade=profundidade
+        self.largura=largura
+
+
+controle_remoto = ControleRemoto("preto", "10cm", "2cm", "2cm") #instanciando uma clas
+print(controle_remoto.cor) #exibindo característica da classe
+
+controle_remoto2 = ControleRemoto("azul", "12cm", "3cm", "4cm")
+print(controle_remoto2.cor)
