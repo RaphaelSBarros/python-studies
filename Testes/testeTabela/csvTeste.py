a12973ae5e0100ea05b8ed6632bfe6eb5a00f611
RAPHAEL ALEXSANDRO DE SOUZA BARROS <DESOUR10@sa.ad.whirlpool.com>
2024-01-03 10:15:35 -0300

    projetinho para mesclar PDFs

--- Testes/testeTabela/csvTeste.py
+++ Testes/testeTabela/csvTeste.py
@@ -2,11 +2,5 @@ import csv
 
 with open("C:/Users/DESOUR10/Downloads/_Aviso de Férias - Mensalistas - Layout_Python (2).csv", 'r') as f:
    reader=csv.reader(f)
-   rows=list(reader)
-   
-for row in rows:
-   row.append('new value')
-   
-with open('tabela.csv', 'w', newline='') as f:
-   writer=csv.writer
-   writer.writerows(rows)
+   for linha in reader:
+      print(linha)
