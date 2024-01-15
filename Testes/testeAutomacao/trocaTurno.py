5281b5da4ff472383a9425a5e1b2dc952b73f4c4
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-01-15 14:41:22 -0300

    Conseguindo escrever os dados a partir da terceira linha da tabela e inserindo datas teste

--- Testes/testeAutomacao/trocaTurno.py
+++ Testes/testeAutomacao/trocaTurno.py
@@ -6,20 +6,20 @@ from openpyxl import workbook, load_workbook
 import subprocess
 import datetime
 import time
+from tkinter import filedialog
 
-file_path = 'C:/Users/DESOUR10/Downloads/Macro Workforce.xlsm' # Escolhe o diretório do arquivo
+
+file_path = filedialog.askopenfilename() # Escolhe o diretório do arquivo
 
 planilha = load_workbook(file_path)
 aba_ativa = planilha.active
 
 # PERCORRE TODAS AS LINHA DA COLUNA C E CASO O VALOR FOR IGUAL A RC002-01 TROCA O VALOR DA LINHA CORRESPONDENTE NA COLUNA E PARA 'NOK'
-for celula in aba_ativa['G']:
-    linha = celula.row
-    aba_ativa[f'E{linha}'] = datetime.time(12,00,00) # Resolvendo problema na hora das fórmulas lerem os valores.
-for celula in aba_ativa['H']:
-    linha = celula.row
-    aba_ativa[f'F{linha}'] = datetime.time(18,15,00) # Caso seja inserido as horas em formato de texto as fórmulas não conseguem ler
-    
+for celula in aba_ativa['C'][2:]:
+    if celula.value != None:
+        linha = celula.row
+        aba_ativa[f'E{linha}'] = datetime.time(12,00,00) # Resolvendo problema na hora das fórmulas lerem os valores.
+        aba_ativa[f'F{linha}'] = datetime.time(18,15,00)
 
 planilha.save("TesteOpenpy.xlsx")
 subprocess.run('TesteOpenpy.xlsx', shell=True)
