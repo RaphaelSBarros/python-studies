dece2324fa6d0a111b17360f9105a861fab4c6e5
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-06-10 10:07:42 -0300

    Corrigindo o problema dos dados em colunas erradas

--- Automacoes/Sz/suporte_sonepar.py
+++ Automacoes/Sz/suporte_sonepar.py
@@ -1,15 +1,20 @@
 from tkinter import filedialog
 from openpyxl import load_workbook, Workbook
+import os
 final_wb = Workbook()
 final_ws = final_wb.active
 final_ws.title = "sonepar_support_report"
 
-#file_path = filedialog.askopenfilename()
-bulk_sla = load_workbook(filename="C:/Users/P0589/Downloads/task_sla.xlsx", data_only=True).active # Arquivo de SLA contendo os dados de itens requisitados e Incidentes
-#file_path = filedialog.askopenfilename()
-incident_data = load_workbook(filename="C:/Users/P0589/Downloads/incident.xlsx", data_only=True).active # Dados dos Incidentes
-#file_path = filedialog.askopenfilename()
-requisiton_data = load_workbook(filename="C:/Users/P0589/Downloads/sc_req_item.xlsx", data_only=True).active # Dados dos Itens Requisitados
+folder_path = filedialog.askdirectory()
+folder = os.listdir(folder_path)
+for file in folder:
+    file_path = os.path.join(folder_path, file)
+    if "task_sla" in file:
+        bulk_sla = load_workbook(filename= file_path, data_only=True).active
+    if "incident" in file:
+        incident_data = load_workbook(filename= file_path, data_only=True).active
+    if "sc_req_item" in file:
+        requisiton_data = load_workbook(filename= file_path, data_only=True).active
 
 # Data de Abrtura / Nº Chamado / Sistema / Cliente / Status (calcular) / Descrição / Atendente / Data Fechamento / Aguardando / Motivo / SLA de Resolução / SLA de Resolução Tempo (calcular)
 titles = ["Dia", "ID Chamado", "Sistema", "Cliente",
@@ -38,10 +43,8 @@ for id_inc in incident_data["B"][1:]: # Percorre todos os IDs de chamados na Col
             x+=1
     for id_chamado in bulk_sla["A"][1:]: # Procura o mesmo Id de chamado dentro da planilha de SLA
         if id_chamado.value == id_inc.value:
-            final_ws.cell(row=new_row, column=x, value=bulk_sla[f"I{id_chamado.row}"].value) # Insere o valor da porcentagem na planilha final
-            x+=1
-            final_ws.cell(row=new_row, column=x, value=(bulk_sla[f"H{id_chamado.row}"].value/86400)) # Faz o cálculo do tempo e insere na planilha final
-
+            final_ws.cell(row=new_row, column=11, value=bulk_sla[f"I{id_chamado.row}"].value) # Insere o valor da porcentagem na planilha final
+            final_ws.cell(row=new_row, column=12, value=(bulk_sla[f"H{id_chamado.row}"].value/86400)) # Faz o cálculo do tempo e insere na planilha final
 
 # Faz a mesma coisa só que para os dados de Itens Requisitados
 for id_ritm in requisiton_data["B"][1:]:
@@ -63,8 +66,6 @@ for id_ritm in requisiton_data["B"][1:]:
             x+=1
     for id_chamado in bulk_sla["A"][1:]:
         if id_chamado.value == id_ritm.value:
-            final_ws.cell(row=new_row, column=x, value=bulk_sla[f"I{id_chamado.row}"].value)
-            x+=1
-            final_ws.cell(row=new_row, column=x, value=(bulk_sla[f"H{id_chamado.row}"].value/86400))
-
-final_wb.save("Sample.xlsx")
+            final_ws.cell(row=new_row, column=11, value=bulk_sla[f"I{id_chamado.row}"].value)
+            final_ws.cell(row=new_row, column=12, value=(bulk_sla[f"H{id_chamado.row}"].value/86400))
+final_wb.save("suporte_sonepar.xlsx")
