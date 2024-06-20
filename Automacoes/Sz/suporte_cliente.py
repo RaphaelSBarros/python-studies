45271e8158a9f01663420c24fe7918e1ac542c0f
RaphaelSBarros <raphaelalexsandr01@gmail.com>
2024-06-20 09:42:28 -0300

    Ajustando o cálculo do SLA de Horas e finalizando a automação

--- Automacoes/Sz/suporte_cliente.py
+++ Automacoes/Sz/suporte_cliente.py
@@ -29,44 +29,52 @@ if yesterday.strftime("%A") == "Sunday":
 for x in range(len(int_titles)):
     int_final_ws.cell(row=1, column=x+1, value=int_titles[x])
 
-for x in range(len(int_titles)):
+for x in range(len(ext_titles)):
     ext_final_ws.cell(row=1, column=x+1, value=ext_titles[x])
 
 for cliente in relatorio["C"][1:]:
-    new_row = int_final_ws.max_row+1
-    if cliente.value == "SZ Soluções":
-        int_final_ws.cell(row=new_row, column=1, value=yesterday)
-        int_final_ws.cell(row=new_row, column=2, value= relatorio[f"A{cliente.row}"].value)
-        int_final_ws.cell(row=new_row, column=3, value= cliente.value)
-        int_final_ws.cell(row=new_row, column=4, value= "algo")
-        int_final_ws.cell(row=new_row, column=5, value= relatorio[f"E{cliente.row}"].value)
-        if relatorio[f"F{cliente.row}"].value in ("Encerrado"):
-            int_final_ws.cell(row=new_row, column=6, value="Fechado")
-        else:
-            int_final_ws.cell(row=new_row, column=6, value="Aberto")
-        int_final_ws.cell(row=new_row, column=7, value= relatorio[f"D{cliente.row}"].value)
-        int_final_ws.cell(row=new_row, column=8, value= relatorio[f"B{cliente.row}"].value)
-        int_final_ws.cell(row=new_row, column=9, value= relatorio[f"G{cliente.row}"].value)
+    int_new_row = int_final_ws.max_row+1
+    ext_new_row = ext_final_ws.max_row+1
 
-        abertura = relatorio[f"O{cliente.row}"].value.split(":")
-        atendimento = relatorio[f"N{cliente.row}"].value.split(":")
-        print(atendimento, abertura)
-        int_final_ws.cell(row=new_row, column=10, value= datetime.timedelta())
+    itime = relatorio[f"O{cliente.row}"].value.split(":")
+    ftime = relatorio[f"N{cliente.row}"].value.split(":")
+
+    idate = relatorio[f"M{cliente.row}"].value.split("/")
+    fdate = relatorio[f"L{cliente.row}"].value.split("/")
+
+    if cliente.value == "SZ Soluções":
+        int_final_ws.cell(row=int_new_row, column=1, value=yesterday)
+        int_final_ws.cell(row=int_new_row, column=2, value= relatorio[f"A{cliente.row}"].value)
+        int_final_ws.cell(row=int_new_row, column=3, value= cliente.value)
+        int_final_ws.cell(row=int_new_row, column=4, value= "algo")
+        int_final_ws.cell(row=int_new_row, column=5, value= relatorio[f"E{cliente.row}"].value)
+        if relatorio[f"F{cliente.row}"].value in ("Encerrado"):
+            int_final_ws.cell(row=int_new_row, column=6, value="Fechado")
+        else:
+            int_final_ws.cell(row=int_new_row, column=6, value="Aberto")
+        int_final_ws.cell(row=int_new_row, column=7, value= relatorio[f"D{cliente.row}"].value)
+        int_final_ws.cell(row=int_new_row, column=8, value= relatorio[f"B{cliente.row}"].value)
+        int_final_ws.cell(row=int_new_row, column=9, value= relatorio[f"G{cliente.row}"].value)
+
+        int_final_ws.cell(row=int_new_row, column=10, value= datetime.datetime(year=int(fdate[2]), month=int(fdate[1]), day=int(fdate[0]), hour=int(ftime[0]), minute=int(ftime[1]), second=int(ftime[2])) - datetime.datetime(year=int(idate[2]), month=int(idate[1]), day=int(idate[0]), hour=int(itime[0]), minute=int(itime[1]), second=int(itime[2])) )
     else:
-        ext_final_ws.cell(row=new_row, column=1, value=yesterday)
-        ext_final_ws.cell(row=new_row, column=2, value= relatorio[f"A{cliente.row}"].value)
+        ext_final_ws.cell(row=ext_new_row, column=1, value=yesterday)
+        ext_final_ws.cell(row=ext_new_row, column=2, value= relatorio[f"A{cliente.row}"].value)
         if "Sonepar" in cliente.value:
             split = [x for x in cliente.value.split(" ") if len(x) > 1]
-            ext_final_ws.cell(row=new_row, column=3, value= split[0])
-            ext_final_ws.cell(row=new_row, column=4, value= split[1])
-        ext_final_ws.cell(row=new_row, column=5, value= relatorio[f"E{cliente.row}"].value)
-        if relatorio[f"F{cliente.row}"].value in ("Encerrado"):
-            ext_final_ws.cell(row=new_row, column=6, value="Fechado")
+            ext_final_ws.cell(row=ext_new_row, column=3, value= split[0])
+            ext_final_ws.cell(row=ext_new_row, column=4, value= split[1])
         else:
-            ext_final_ws.cell(row=new_row, column=6, value="Aberto")
-        ext_final_ws.cell(row=new_row, column=7, value= relatorio[f"B{cliente.row}"].value)
-        ext_final_ws.cell(row=new_row, column=8, value= relatorio[f"D{cliente.row}"].value)
-        ext_final_ws.cell(row=new_row, column=9, value= relatorio[f"G{cliente.row}"].value)
-        ext_final_ws.cell(row=new_row, column=10, value= datetime.timedelta())
+            ext_final_ws.cell(row=ext_new_row, column=3, value="BeeStock")
+            ext_final_ws.cell(row=ext_new_row, column=4, value= relatorio[f"C{cliente.row}"].value)
+        ext_final_ws.cell(row=ext_new_row, column=5, value= relatorio[f"E{cliente.row}"].value)
+        if relatorio[f"F{cliente.row}"].value in ("Encerrado"):
+            ext_final_ws.cell(row=ext_new_row, column=6, value="Fechado")
+        else:
+            ext_final_ws.cell(row=ext_new_row, column=6, value="Aberto")
+        ext_final_ws.cell(row=ext_new_row, column=7, value= relatorio[f"B{cliente.row}"].value)
+        ext_final_ws.cell(row=ext_new_row, column=8, value= relatorio[f"D{cliente.row}"].value)
+        ext_final_ws.cell(row=ext_new_row, column=9, value= relatorio[f"G{cliente.row}"].value)
+        ext_final_ws.cell(row=ext_new_row, column=10, value= datetime.datetime(year=int(fdate[2]), month=int(fdate[1]), day=int(fdate[0]), hour=int(ftime[0]), minute=int(ftime[1]), second=int(ftime[2])) - datetime.datetime(year=int(idate[2]), month=int(idate[1]), day=int(idate[0]), hour=int(itime[0]), minute=int(itime[1]), second=int(itime[2])) )
 
 final_wb.save(f"data/suporte_cliente.xlsx")
